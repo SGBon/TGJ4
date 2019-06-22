4635da46c591ab583fe760438451dbdf04a51944
Santiago Bonada <Santiago.Bonada@uoit.net>
2019-06-22 01:43:45 -0400

    Part of 2HU48 implemented. Can only go left and right. Big refactoring needed before doing up and down

---
+++ Assets/Scenes/2HU48/Scripts/TileController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+	public TextMesh valueLabel;
+	private int value = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void setValue(int value)
+	{
+		this.value = value;
+		valueLabel.text = ""+value;
+	}
+
+	public int getValue()
+	{
+		return value;
+	}
+}
