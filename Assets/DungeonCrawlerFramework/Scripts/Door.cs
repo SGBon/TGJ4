ee28f3413f34fa40a0f539f8b71d79fdc1f9c7a5
Jacky Yang <jackyyang267@gmail.com>
2019-06-22 11:58:31 -0400

    Fixed some dungeon crawling bugs

--- Assets/DungeonCrawlerFramework/Scripts/Door.cs
+++ Assets/DungeonCrawlerFramework/Scripts/Door.cs
@@ -13,10 +13,10 @@ public class Door : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //void Update()
+    //{
+    //    
+    //}
 
     public void OpenDoor()
     {
