e0a4d92e7b55a974887ec4841bf4598c95256cb8
prototype-A <20850333+prototype-A@users.noreply.github.com>
2019-06-23 09:49:11 -0400

    Import new character portraits as Sprites (2D and UI), Add and complete Labyrinth Floor 2 VN story, Change some beginning and Labyrinth floor 1 character portraits

---
+++ Assets/DemoVN/CustomButtonFunctions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomButtonFunctions : MonoBehaviour
+{
+    // Quit Button
+    public void Quit()
+    {
+        // Quit the game application
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+}
